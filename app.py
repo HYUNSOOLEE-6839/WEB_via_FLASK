fe499b78bee08b84fe935d3a077de746f0e72f58
HYUNSOOLEE-6839 <70185551+HYUNSOOLEE-6839@users.noreply.github.com>
2021-04-02 15:37:53 +0900

    210402
    하나씩 만들어가는 Flask Web

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    menu = {'ho':1, 'da':0, 'ml':0, 
+            'se':0, 'co':0, 'cg':0, 'cr':0, 'wc':0,
+            'cf':0, 'ac':0, 're':0, 'cu':0}
+    return render_template('index.html', menu=menu)
